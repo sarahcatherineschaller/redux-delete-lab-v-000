da28d45c3a0cdcbbd36fdee68623ef4cb86f6dcd
Sarah Schaller <sarahcschaller@gmail.com>
2018-10-31 23:28:14 +0000

    automatically backed up by learn

--- src/components/BandsContainer.js
+++ src/components/BandsContainer.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
 import BandInput from './BandInput';
-
-import { connect } from 'react-redux'
+import { connect } from 'react-redux';
+import Bands from './Bands';
 
 class BandsContainer extends Component {
 
